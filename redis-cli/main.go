c0feb7ad60385b8dfc7ff84a99883d8528202ad1
ernesto <ernestoponce27@gmail.com>
2024-08-10 22:17:30 -0300

    update

--- redis-cli/main.go
+++ redis-cli/main.go
@@ -4,46 +4,132 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"strings"
+	"time"
+
+	"github.com/eiannone/keyboard"
 )
 
-func main() {
-	// Connect to Redis server
+type RedisClient struct {
+	conn net.Conn
+	host string
+	port string
+}
+
+func NewRedisClient(host string, port string) (*RedisClient, error) {
 	conn, err := net.Dial("tcp", "localhost:6379")
 	if err != nil {
 		fmt.Println("Error connecting to Redis:", err)
-		return
+		return nil, err
+	}
+	return &RedisClient{
+		conn: conn,
+		host: host,
+		port: port,
+	}, nil
+}
+
+func (r *RedisClient) Close() {
+	r.conn.Close()
+}
+
+func (r *RedisClient) buildCommand(args ...string) string {
+	command := "*" + fmt.Sprint(len(args)) + "\r\n"
+	for _, arg := range args {
+		command += "$" + fmt.Sprint(len(arg)) + "\r\n" + arg + "\r\n"
 	}
 
-	// Send a PING command to Redis using RESP protocol
-	// command := "*1\r\n$4\r\nping\r\n"
-	// command := "*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n"
-	command := "*2\r\n$3\r\nget\r\n$3\r\nkey\r\n"
-	_, err = conn.Write([]byte(command))
+	return command
+}
+
+func (r *RedisClient) execute(args ...string) (string, error) {
+	command := r.buildCommand(args...)
+
+	_, err := r.conn.Write([]byte(command))
 	if err != nil {
-		fmt.Println("Error sending command to Redis:", err)
-		return
+		return "", err
 	}
 
-	// Read the response
-	reader := bufio.NewReader(conn)
+	r.conn.SetReadDeadline(time.Now().Add(3 * time.Second))
+	reader := bufio.NewReader(r.conn)
 	response, err := reader.ReadString('\n')
-	fmt.Println(response)
 	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return
+		return "", err
 	}
 
-	// if strings.HasPrefix(response, "$") {
-	// 	// Read the actual bulk string data
-	// 	data, err := reader.ReadString('\n')
-	// 	if err != nil {
-	// 		fmt.Println("Error reading bulk string data:", err)
-	// 		return
-	// 	}
-	// 	response = data
-	// }
+	if strings.HasPrefix(response, "$") {
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+		response = data
+	}
 
-	// Print the response
-	fmt.Println("Response from Redis:", strings.TrimSpace(response))
+	return response, nil
+}
+
+func main() {
+	// Connect to Redis server
+	redisClient, err := NewRedisClient("localhost", "6379")
+	if err != nil {
+		panic(err)
+	}
+	defer redisClient.Close()
+
+	if err := keyboard.Open(); err != nil {
+		fmt.Println("Error opening keyboard:", err)
+		os.Exit(1)
+	}
+	defer keyboard.Close()
+
+	var value string
+	for {
+		fmt.Print("\033[2K\033[G")
+
+		suggest := "\033[90msuggest\033[0m"
+		fmt.Print("$localhost:6379> ", value+" "+suggest)
+		var err error
+		var key keyboard.Key
+
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			fmt.Println("Error getting key:", err)
+			break
+		}
+		//fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
+
+		switch key {
+		case keyboard.KeyEnter:
+			parts := strings.Fields(value)
+
+			if len(parts) == 0 {
+				continue
+			}
+
+			resp, err := redisClient.execute(parts...)
+			if err != nil {
+				fmt.Println("Error executing command:", err)
+				continue
+			}
+			fmt.Println()
+			fmt.Println(resp)
+			value = ""
+
+		case keyboard.KeyEsc:
+			fmt.Println("ESC pressed. Exiting...")
+			return
+
+		case keyboard.KeyBackspace2:
+			if len(value) != 0 {
+				value = value[:len(value)-1]
+			}
+		case keyboard.KeySpace:
+			value += " "
+
+		default:
+			value += string(char)
+		}
+
+	}
 }
