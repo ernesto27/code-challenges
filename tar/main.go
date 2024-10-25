8c27391cb43a9f17400d7a3961080e6f01f60679
ernesto <ernesto@pigmalion.com.ar>
2024-10-24 23:01:07 -0300

    update tar

--- tar/main.go
+++ tar/main.go
@@ -6,14 +6,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"syscall"
 )
 
 type Tar struct {
-	file *os.File
+	file       *os.File
+	newTarName string
 }
 
-func NewTar(filename string) (*Tar, error) {
-	file, err := os.Open(filename)
+func NewTar(tarName string, create bool) (*Tar, error) {
+	if create {
+		return &Tar{newTarName: tarName}, nil
+	}
+
+	file, err := os.Open(tarName)
 	if err != nil {
 		return nil, err
 	}
@@ -59,35 +65,94 @@ func (t *Tar) ExtractFiles() error {
 			return err
 		}
 
-		if header.Typeflag == tar.TypeReg { // Regular file
-			fmt.Printf("File: %s\n", header.Name)
-			content, err := io.ReadAll(tarReader)
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Content:\n%s\n", string(content))
-			fmt.Println("------------------------")
+		content, err := io.ReadAll(tarReader)
+		if err != nil {
+			return err
+		}
+		file, err := os.Create(header.Name)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = file.Write(content)
+		if err != nil {
+			return err
 		}
 	}
 
 	return nil
 }
 
+func (t *Tar) CreateTar(args []string) error {
+	fmt.Println(args)
+	newTarFile, err := os.Create(t.newTarName)
+	if err != nil {
+		return err
+	}
+	defer newTarFile.Close()
+
+	tarWriter := tar.NewWriter(newTarFile)
+
+	for _, arg := range args {
+		file, err := os.Open(arg)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		stat, err := file.Stat()
+		if err != nil {
+			return err
+		}
+
+		sysStat, ok := stat.Sys().(*syscall.Stat_t)
+		if !ok {
+			return fmt.Errorf("failed to get system-specific file info for %s", arg)
+		}
+
+		header := &tar.Header{
+			Name:    file.Name(),
+			Size:    stat.Size(),
+			Mode:    int64(stat.Mode()),
+			ModTime: stat.ModTime(),
+			Uid:     int(sysStat.Uid),
+			Gid:     int(sysStat.Gid),
+		}
+
+		if err := tarWriter.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if _, err := io.Copy(tarWriter, file); err != nil {
+			return err
+		}
+	}
+
+	if err := tarWriter.Close(); err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
 func main() {
 	t := flag.Bool("t", false, "list files for stdin")
 	tf := flag.Bool("tf", false, "list files")
 	xf := flag.Bool("xf", false, "extract files")
+	cf := flag.Bool("cf", false, "create tar file")
 
 	flag.Parse()
 
-	filename := ""
-	if *tf || *xf {
-		args := flag.Args()
+	tarName := ""
+	args := flag.Args()
+
+	if *tf || *xf || *cf {
 		if len(args) == 0 {
-			fmt.Println("Usage: tar <filename>")
+			fmt.Println("Usage: tar <tarName>")
 			os.Exit(1)
 		}
-		filename = args[0]
+		tarName = args[0]
 	} else if *t {
 		stdContent, err := io.ReadAll(os.Stdin)
 		if err != nil {
@@ -106,10 +171,10 @@ func main() {
 			fmt.Println("Error writing to temporary file:", err)
 			os.Exit(1)
 		}
-		filename = tmpfile.Name()
+		tarName = tmpfile.Name()
 	}
 
-	tar, err := NewTar(filename)
+	tar, err := NewTar(tarName, *cf)
 	if err != nil {
 		fmt.Println("Error opening tar file: ", err)
 		os.Exit(1)
@@ -127,6 +192,12 @@ func main() {
 		err := tar.ListFiles()
 		if err != nil {
 			fmt.Println("Error listing files:", err)
+			os.Exit(1)
+		}
+	} else if *cf {
+		err := tar.CreateTar(args[1:])
+		if err != nil {
+			fmt.Println("Error creating tar file:", err)
 			os.Exit(1)
 		}
 	}
